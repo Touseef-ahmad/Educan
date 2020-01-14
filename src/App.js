b31d629bba431af866a2c8395322bcb903e2143a
Touseef Ahmad <touseef.ahmad.cs@gmail.com>
2020-01-14 16:44:14 +0500

    removed user view from sidenav bar

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component{
         
   <nav>
     <div className="nav-wrapper" style={{backgroundColor:"#3b5998"} }>
-      <a href="#" className="brand-logo center">Logo</a>
+      <a href="#" className="brand-logo center">Educan</a>
       <ul id="nav-mobile" className="left hide-on-med-and-down">
         <li><a href="sass.html">Sass</a></li>
         <li><a href="badges.html">Components</a></li>
@@ -19,14 +19,6 @@ class App extends Component{
   </nav>
         
   <ul id="slide-out" className="sidenav">
-    <li><div className="user-view">
-      <div className="background">
-        <img src="images/office.jpg"/>
-      </div>
-      <a href="#user"><img className="circle" src="images/yuna.jpg"/></a>
-      <a href="#name"><span className="white-text name">John Doe</span></a>
-      <a href="#email"><span className="white-text email">jdandturk@gmail.com</span></a>
-    </div></li>
     <li><a href="#!"><i className="material-icons">cloud</i>First Link With Icon</a></li>
     <li><a href="#!">Second Link</a></li>
     <li><div className="divider"></div></li>
